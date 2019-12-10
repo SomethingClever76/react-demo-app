9a8aaa3ab08fd4dfe85caac58b79150c78014ff4
Thomas <thmasky@amazon.com>
2019-12-10 17:06:26 -0500

    Added Jeopardy component, route, link. Started working on game function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Welcome from './components/welcome/Welcome';
 import Clock from './components/clock/Clock';
-import Contact from './components/contact/Contact'
-import NoMatch from './components/nomatch/NoMatch'
+import Contact from './components/contact/Contact';
+import Jeopardy from './components/jeopardy/Jeopardy';
+import NoMatch from './components/nomatch/NoMatch';
 //Import the Navigation component
 import Navigation from './components/navigation/Navigation';
 //Import the Route component
@@ -34,6 +35,7 @@ function App() {
         />
         <Route path="/clock" component={Clock} />
         <Route path="/contact" component={Contact} />
+        <Route path="/jeopardy" component={Jeopardy} />
         <Route>
           <NoMatch />
         </Route>
---
+++ src/components/jeopardy/Jeopardy.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './Jeopardy.css'
+//import our service
+import JeopardyService from "../../jeopardyService";
+class Jeopardy extends Component {
+  //set our initial state and set up our service as this.client on this component
+  constructor(props){
+    super(props);
+    this.client = new JeopardyService();
+    this.state = {
+      data: {},
+      score: 0
+    }
+  }
+  //get a new random question from the API and add it to the data object in state
+  getNewQuestion() {
+    return this.client.getQuestion().then(result => {
+      this.setState({
+        data: result.data[0]
+      })
+    })
+  }
+  //when the component mounts, get a the first question
+  componentDidMount() {
+    this.getNewQuestion();
+  }
+  //display the results on the screen
+  render() {
+    return (
+      <div className="jeopardyClue">
+        <h2>
+        {/* {JSON.stringify(this.state.data)} */}
+        Question: {this.state.data.question} <br/>
+        <br/>
+        <br/>
+        <br/>
+        Value: {this.state.data.value}  <br/>
+        {/* Category: {this.state.data.category.title} */}
+        </h2>
+      </div>
+    );
+  }
+}
+export default Jeopardy;
